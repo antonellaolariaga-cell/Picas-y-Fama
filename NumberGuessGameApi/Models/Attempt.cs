5335235454b8518f5a6e327796281bf2f16f8d1d
Anto Olariaga <Antoolariaga@outlook.com>
2026-06-30 18:57:39 -0300

    Se agrega entidades base del modelo de datos

---
+++ NumberGuessGameApi/Models/Attempt.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NumberGuessGameApi.Models
+{
+    public class Attempt
+    {
+        [Key]
+        public int AttemptId { get; set; }
+
+        [Required]
+        public int GameId { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string AttemptedNumber { get; set; } = string.Empty;
+
+        [Required]
+        public string ResultMessage { get; set; } = string.Empty; // Guardará el texto de Famas/Picas
+
+        [Required]
+        public DateTime AttemptedAt { get; set; } = DateTime.UtcNow;
+
+        // Propiedad de navegación
+        [ForeignKey("GameId")]
+        public Game Game { get; set; } = null!;
+    }
+}
+
